d5b9d783d538ef8285cf01c5b2c79bf19e614452
Hye Soo Park <flyjwayur@gmail.com>
2019-03-20 13:33:19 +0200

    Configure a bridge between backend and frontend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5007;
 const formRouter = require('./server/routes/formRoutes/formRoutes');
@@ -21,6 +22,16 @@ app.get('/', (req, res) => {
 
 // REST API end points under '/'
 app.use('/api/formData', formRouter);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+if (process.env.node_env === 'production') {
+  //The catchall handler : if some request that does not match, send back React's index.html file
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
